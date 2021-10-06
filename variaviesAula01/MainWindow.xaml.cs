1992bd27744f89ba872c96fc86b4f2727df08a3f
PabloMana <pablommana@hotmail.com>
2021-10-06 17:16:10 -0300

    Criação da calculadora

---
+++ variaviesAula01/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace variaviesAula01
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsomar_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Declaraão das variaveis
+            double N1, N2, Resultado;
+           
+            //Entrada de dados
+            N1 = Convert.ToDouble(txtNumero1.Text); //efetuando a conversão de string para double
+            N2 = Convert.ToDouble(txtNumero2.Text);
+
+            //Efetuando o cálculo
+            Resultado = N1 + N2;
+
+            //Saída de dados
+            lblResultado.Content = "Somar: " + Resultado.ToString();
+
+
+        }
+
+        private void btnSubtrair_Click(object sender, RoutedEventArgs e)
+        {
+            //Declaraão das variaveis
+            double N1, N2, Resultado;
+
+            //Entrada de dados
+            N1 = Convert.ToDouble(txtNumero1.Text); //efetuando a conversão de string para double
+            N2 = Convert.ToDouble(txtNumero2.Text);
+
+            //Efetuando o cálculo
+            Resultado = N1 - N2;
+
+            //Saída de dados
+            lblResultado.Content = "Subtrair: " + Resultado.ToString();
+        }
+
+        private void btnDividir_Click(object sender, RoutedEventArgs e)
+        {
+            //Declaraão das variaveis
+            double N1, N2, Resultado;
+
+            //Entrada de dados
+            N1 = Convert.ToDouble(txtNumero1.Text); //efetuando a conversão de string para double
+            N2 = Convert.ToDouble(txtNumero2.Text);
+
+            //Efetuando o cálculo
+            Resultado = N1 / N2;
+
+            //Saída de dados
+            lblResultado.Content = "Dividir: " + Resultado.ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, RoutedEventArgs e)
+        {
+            //Declaraão das variaveis
+            double N1, N2, Resultado;
+
+            //Entrada de dados
+            N1 = Convert.ToDouble(txtNumero1.Text); //efetuando a conversão de string para double
+            N2 = Convert.ToDouble(txtNumero2.Text);
+
+            //Efetuando o cálculo
+            Resultado = N1 * N2;
+
+            //Saída de dados
+            lblResultado.Content = "Multiplicar: " + Resultado.ToString();
+        }
+    }
+}
